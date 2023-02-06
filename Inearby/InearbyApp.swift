cc7f01acfe5299f961b0e1024384df9853e15e5d
Ibtisam <ihowaimel@gmail.com>
2023-02-06 16:05:26 +0300

    Initial Commit

---
+++ Inearby/InearbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  InearbyApp.swift
+//  Inearby
+//
+//  Created by Ibtisam on 15/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct InearbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
