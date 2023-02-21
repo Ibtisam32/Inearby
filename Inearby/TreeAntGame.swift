6822800de9fc67e690813e012a64ceafc62375eb
Kawthar Almajhad <kawtharappledev@gmail.com>
2023-02-21 15:38:29 +0300

    - one tap - popup

--- Inearby/TreeAntGame.swift
+++ Inearby/TreeAntGame.swift
@@ -14,55 +14,72 @@ struct TreeAntGame: View {
     @State private var FirstUserScore = 0
     @State var SecondUserScore = 0
     @State private var timerC: Timer?  //nil
+    @State private var exitGamePopup = false
     
     var body: some View {
         ZStack{LinearGradient(colors: [.init("BabyBlue")], startPoint: .zero, endPoint: .zero).ignoresSafeArea()
                 .overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.init("DarkBlue"),lineWidth: 6).frame(width: 350,height: 700))
-            
+
             HStack{
                 Text("\(FirstUserScore)").padding(70)
                 Text(":")
                 Text("\(SecondUserScore)").padding(70)
             }.offset(y:-280).foregroundColor(.white).font(.system(size: 30, weight: .bold))
             
-            Button("Exit Game") {
+            Button(action: {
+                withAnimation(.linear(duration: 0.1)) {
+                    self.exitGamePopup = true
+                }
                 
-            }.frame(width: 300, height: 60)
+            }, label: {
+                Text("Exit Game")
+            }).frame(width: 300, height: 60)
                 .background(Color.init("DarkBlue"))
                 .foregroundColor(Color.white)
                 .cornerRadius(10)
                 .font(.system(size: 30, weight: .bold))
                 .multilineTextAlignment(.center).offset(y:280)
             
-            
-        HStack{
-            Button(action: {
-                if self.timerC == nil && text == "Tree" { //If the timer is nil, it means >> no previous press has happened in the current interval
-                    self.FirstUserScore += 1
-                    self.timerC = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false) { timer in
-                        self.timerC = nil
-                    }
+            VStack {
+                Spacer().frame(height:160)
+                HStack{
+                    Button(action: {
+                        if self.timerC == nil && text == "Tree" { //If the timer is nil, it means >> no previous press has happened in the current interval
+                            self.FirstUserScore += 1
+                            self.timerC = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false) { timer in
+                                self.timerC = nil
+                            }
+                        }
+                    }){
+                        Image("Tree").resizable()
+                            .aspectRatio(contentMode: .fill).frame(width:130 ,height: 180).background(Color.init("DarkBlue")).cornerRadius(15)
+                    }.padding()
+                    
+                    Button(action: {
+                        if self.timerC == nil && text == "Ant" { //If the timer is nil, it means >> no previous press has happened in the current interval
+                            self.FirstUserScore += 1
+                            self.timerC = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false) { timer in
+                                self.timerC = nil
+                            }
+                        }
+                        //                else if FirstUserScore == 10 {
+                        //                    showingPopup = true
+                        //                }
+                    }){
+                        Image("Ant").resizable()
+                            .aspectRatio(contentMode: .fit).frame(width:130 ,height: 180).background(Color.init("Blue")).cornerRadius(15)
+                        
+                    }.padding()
                 }
-            }){
-                Image("Tree").resizable()
-                    .aspectRatio(contentMode: .fill).frame(width:130 ,height: 180).background(Color.init("DarkBlue")).cornerRadius(15)
-            }.padding()
+            }
             
-            Button(action: {
-                if self.timerC == nil && text == "Ant" { //If the timer is nil, it means >> no previous press has happened in the current interval
-                    self.FirstUserScore += 1
-                    self.timerC = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false) { timer in
-                        self.timerC = nil
-                    }
-                }
-            }){
-                Image("Ant").resizable()
-                    .aspectRatio(contentMode: .fit).frame(width:130 ,height: 180).background(Color.init("Blue")).cornerRadius(15)
-                
-            }.padding()
-        }.offset(y:100)
             
-          
+                 if $exitGamePopup.wrappedValue {
+                     
+                     ExitPopupfunc()
+                 }
+                 
+              
                 Text(text)
                     .frame(width: 150, height: 60)
                     .background(Color.init("DarkBlue"))
@@ -76,7 +93,61 @@ struct TreeAntGame: View {
                         }// self.text = bool? true : false
                     }
             
+//            if FirstUserScore == 10 {
+//                Text("End Game")
+//            }
+
+            
         }
+    }
+    
+    private func ExitPopupfunc() -> some View {
+        
+        VStack (spacing : 5) {
+            Text("Exit?").font(.system(size: 24, weight: .bold)).foregroundColor(Color.init("DarkBlue")).padding()
+            ZStack {
+                RoundedRectangle(cornerRadius: 15).fill(Color.init("BabyGray")).frame(width: 260, height: 120)
+                Text("Are you sure you want to quit the game?").font(.headline).foregroundColor(Color.init("DarkBlue")).multilineTextAlignment(.center)
+                
+            }
+            HStack{
+                
+                Button(action: {
+                    
+                    withAnimation {
+                        
+                        self.exitGamePopup = false
+                        
+                    }
+                }, label: {
+                    Text("Yes").frame(width: 100, height: 40)
+                        .background(Color.green)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(10)
+                }).padding()
+                
+                Button(action: {
+                    
+                    withAnimation {
+                        
+                        self.exitGamePopup = false
+                        
+                    }
+                }, label: {
+                    Text("No").frame(width: 100, height: 40)
+                        .background(Color.red)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(10)
+                }).padding()
+            }
+        }
+        
+        .padding()
+        .frame(width: 300, height: 270)
+        .background(Color.init("PopupColor"))
+        .cornerRadius(20)
+        //.shadow(radius: 30 )
+    
     }
     
 }
