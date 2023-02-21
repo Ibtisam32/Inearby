cf54438d000c7b7b3969bae3bc3fbdc004ad5222
alanoud <alanuodsaleeh@gmail.com>
2023-02-21 17:55:44 +0300

    info.plist and entitlements

---
+++ Inearbyy/InearbyyApp.swift
@@ -0,0 +1,17 @@
+//
+//  InearbyyApp.swift
+//  Inearbyy
+//
+//  Created by Alanoud Saleh on 01/08/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct InearbyyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
